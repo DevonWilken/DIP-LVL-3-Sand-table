fbf353b43bacca03cbf5fb610ddec74b8792107d
DevonWilken <78832711+DevonWilken@users.noreply.github.com>
2022-05-16 23:53:10 +0000

    V1

---
+++ Gcode_sender/Gcode_Sender_V1_light_test.py
@@ -0,0 +1,14 @@
+import serial
+import time
+serialcomm = serial.Serial("COM 4, 9600")
+serialcomm.timeout = 1
+
+while True:
+    i = input("input on /off").strip()
+    if i == "done":
+        print("finished programme")
+        break
+    serialcomm.wirte(i.encode())
+    time.sleep(0.5)
+    print(serialcomm.readline().decode("ascii"))
+serialcomm.close()
